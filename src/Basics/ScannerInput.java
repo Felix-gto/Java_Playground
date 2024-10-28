c635cf653b74d2160768df6234835b160836955c
Felix <felix.daraban@gmail.com>
2024-10-28 21:44:10 +0200

    Updated scanner - added Integer input

--- src/Basics/ScannerInput.java
+++ src/Basics/ScannerInput.java
@@ -5,11 +5,19 @@ public class ScannerInput {
 
     public static void main(String[] args) {
 
+        // Create the Scanner object
         Scanner myScanner = new Scanner(System.in);
+
+        // Input a String
         System.out.println("What is your name?");
 
         String nameInput = myScanner.nextLine();
         System.out.println("Hello, " + nameInput);
 
+        // Input an integer
+        System.out.println("How old are you?");
+
+        int ageInput = myScanner.nextInt();
+        System.out.println(nameInput + " is " + ageInput + " years old");
     }
 }
