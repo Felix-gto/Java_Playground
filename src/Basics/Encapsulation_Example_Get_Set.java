409a33fbe9505412b8c69d2cb05d5be6a84ae91b
Felix <felix.daraban@gmail.com>
2025-03-07 07:01:38 +0200

    Encapsulation - minor refactoring

--- src/Basics/Encapsulation_Example_Get_Set.java
+++ src/Basics/Encapsulation_Example_Get_Set.java
@@ -1,23 +1,22 @@
 package Basics;
 
+class Person {
+    private String name; // Private variable (hidden from outside)
+
+    // Public method to set the name (write access)
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    // Public method to get the name (read access)
+    public String getName() {
+        return name;
+    }
+}
+
 public class Encapsulation_Example_Get_Set {
 
     public static void main(String[] args) {
-
-        class Person {
-            private String name; // Private variable (hidden from outside)
-
-            // Public method to set the name (write access)
-            public void setName(String newName) {
-                name = newName;
-            }
-
-            // Public method to get the name (read access)
-            public String getName() {
-                return name;
-            }
-        }
-
         Person person = new Person();
         person.setName("Alice");  // Setting the name
         System.out.println(person.getName()); // Getting the name → Output: Alice
